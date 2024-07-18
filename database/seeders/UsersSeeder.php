5a7eeec7b9fec33a6af8d29b33293bbee74c6658
romis <romis.nesmelov@gmail.com>
2024-07-18 19:13:59 +0300

    changed validation texts

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -47,47 +47,47 @@ class UsersSeeder extends Seeder
 //                'admin' => 1,
 //                'active' => 1
 //            ],
-            [
-                'name' => 'Unknown',
-                'family' => 'Unknown',
-                'born' => '14-07-1976',
-                'phone' => '+7(925)521-37-45',
-                'email' => 'unknown@mail.ru',
-                'password' => bcrypt('123456'),
-                'code' => '99-99-99',
-                'admin' => 1,
-                'active' => 1
-            ],
-            [
-                'name' => 'Anonymous',
-                'family' => 'Anonymous',
-                'phone' => '+7(926)206-39-77',
-                'born' => '14-07-1976',
-                'email' => 'anonymous@mail.ru',
-                'password' => bcrypt('123456'),
-                'code' => '99-99-99',
-                'admin' => 1,
-                'active' => 1
-            ]
+//            [
+//                'name' => 'Unknown',
+//                'family' => 'Unknown',
+//                'born' => '14-07-1976',
+//                'phone' => '+7(925)521-37-45',
+//                'email' => 'unknown@mail.ru',
+//                'password' => bcrypt('123456'),
+//                'code' => '99-99-99',
+//                'admin' => 1,
+//                'active' => 1
+//            ],
+//            [
+//                'name' => 'Anonymous',
+//                'family' => 'Anonymous',
+//                'phone' => '+7(926)206-39-77',
+//                'born' => '14-07-1976',
+//                'email' => 'anonymous@mail.ru',
+//                'password' => bcrypt('123456'),
+//                'code' => '99-99-99',
+//                'admin' => 1,
+//                'active' => 1
+//            ]
         ];
 
         foreach ($data as $item) {
             User::create($item);
         }
 
-        for ($i=1;$i<=15;$i++) {
-            User::create([
-                'name' => 'User'.($i),
-                'family' => 'Test'.($i),
-                'phone' => '+7(926)111-11-'.($i < 10 ? '0'.$i : $i),
-                'born' => '01-01-1970',
-                'email' => 'user'.($i).'@mail.ru',
-                'password' => bcrypt('123456'),
-                'code' => '99-99-99',
-                'admin' => 1,
-                'active' => 1
-            ]);
-        }
+//        for ($i=1;$i<=15;$i++) {
+//            User::create([
+//                'name' => 'User'.($i),
+//                'family' => 'Test'.($i),
+//                'phone' => '+7(926)111-11-'.($i < 10 ? '0'.$i : $i),
+//                'born' => '01-01-1970',
+//                'email' => 'user'.($i).'@mail.ru',
+//                'password' => bcrypt('123456'),
+//                'code' => '99-99-99',
+//                'admin' => 1,
+//                'active' => 1
+//            ]);
+//        }
 //        User::factory(10)->create();
     }
 }
