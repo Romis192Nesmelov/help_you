27deef2cf932a3193e31f2f2b9cd0231858981e1
Romis <romis@nesmelov.com>
2024-10-22 12:16:23 +0300

    small fixes

--- resources/views/emails/new_message_notice.blade.php
+++ resources/views/emails/new_message_notice.blade.php
@@ -3,5 +3,5 @@
 @section('content')
     @include('blocks.email_head_block')
     <h3>{{ trans('mail.new_message',['name' => $order->name]) }}</h3>
-    @include('blocks.to_go_to_the_chat_block',['id' => $order->id])
+    @include('blocks.to_go_to_the_chat_block',['order_id' => $order->id])
 @endsection
